c96455a116697c707fb77cdd6e23dae680e49162
anickel101 <anson.nickel@gmail.com>
2020-10-30 21:41:59 -0400

    10-30 pm

--- src/Containers/MapContainer.js
+++ src/Containers/MapContainer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Map, GoogleApiWrapper, Marker} from 'google-maps-react';
+import {Map, GoogleApiWrapper, Marker, InfoWindow} from 'google-maps-react';
 import mapStyle from "../Options/MapStyles"
  
 const initialCenter = {
@@ -18,7 +18,8 @@ class MapContainer extends React.Component {
 
   state={
     focus_marker: {},
-    viewable_buildings: []
+    viewable_buildings: [],
+    selected: null
   }
 
   componentDidMount() {
@@ -42,13 +43,30 @@ class MapContainer extends React.Component {
     })
   }
 
-  markerClick = (props, marker, event) => {
-  }
-
   renderBuildings = () => {
     if (this.state.viewable_buildings) {
-      return this.state.viewable_buildings.map(bldg => <Marker key={bldg.bin} position={{lat: bldg.gis_lat, lng: bldg.gis_long}} />)
+      return this.state.viewable_buildings.map(bldg => <Marker onClick={this.setSelected} key={bldg.bin} info={bldg} position={{lat: bldg.gis_lat, lng: bldg.gis_long}} />)
     }
+  }
+
+  renderSelectedToolTip = () => {
+    console.log("Inside renderSelected")
+    return (
+      <InfoWindow marker={this.state.selected} visible={true} onClose={this.clearSelected}>
+        <div>
+          <h2>Building Here!</h2>
+          <p>{this.state.selected.info.bin}</p>
+        </div>
+    </InfoWindow>
+    )
+  }
+
+  setSelected = (props, marker, event) => {
+    this.setState({selected: marker})
+  }
+
+  clearSelected = () => {
+    this.setState({selected: null})
   }
 
   render() {
@@ -58,15 +76,15 @@ class MapContainer extends React.Component {
         containerStyle={mapContainerDims}
         initialCenter={initialCenter}
         center={this.state.focus_marker}
-        zoom={18}
+        zoom={16}
         onReady={(mapProps, map) => this._mapLoaded(mapProps, map)}
         onClick={this.handleClick}
         >
 
-        <Marker onClick={this.markerClick} position={{lat: this.state.focus_marker.lat, lng: this.state.focus_marker.lng}} />
+        <Marker onClick={this.markerClick} icon={{url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"}} position={{lat: this.state.focus_marker.lat, lng: this.state.focus_marker.lng}} />
 
         {this.renderBuildings()}
-
+        {this.state.selected ? (this.renderSelectedToolTip()) : null}
       </Map>
 
     );
